b1f44b7febac50568611d117d7e9d67a52aa766f
Matias Klemola <matias.klemola@gmail.com>
2019-05-16 15:57:14 +0300

    Add external posts list

--- content/data/pelicandata.py
+++ content/data/pelicandata.py
@@ -10,5 +10,8 @@ SOCIAL_LINKS = (('GitHub', 'http://github.com/klemola'),
                 ('Soundcloud', 'https://soundcloud.com/butsku'),
                 ('Twitter', 'https://twitter.com/MatiasKlemola'))
 
+EXTERNAL_POSTS = [('“I want to host a meetup” @ Valuemotive blog',
+                   'https://blog.valuemotive.com/i-want-to-host-a-meetup-33255235ecf')]
+
 SITE_SOURCE_CODE_URL = 'https://github.com/klemola/matiasklemola.com'
 CURRENT_YEAR = now.year
