de885ee83656df823742970b04f3a62db97b2f39
Matias Klemola <matias.klemola@gmail.com>
2023-10-19 14:08:11 +0300

    Update external article links

--- content/data/pelicandata.py
+++ content/data/pelicandata.py
@@ -11,8 +11,9 @@ SOCIAL_LINKS = (('GitHub', 'http://github.com/klemola'),
                 ('Soundcloud', 'https://soundcloud.com/butsku'),
                 ('Mastodon', 'https://mastodon.gamedev.place/@yourmagicisworking'))
 
-EXTERNAL_ARTICLES = [('“I want to host a meetup” @ Valuemotive blog',
-                      'https://blog.valuemotive.com/i-want-to-host-a-meetup-33255235ecf')]
+EXTERNAL_ARTICLES = [('“I want to host a meetup”',
+                      'https://www.valuemotive.com/post/i-want-to-host-a-meetup'),
+                     ("Hobby project spotlight: “vetovoima”", 'https://www.valuemotive.com/post/hobby-project-spotlight-vetovoima')]
 
 SITE_SOURCE_CODE_URL = 'https://github.com/klemola/matiasklemola.com'
 CURRENT_YEAR = now.year
